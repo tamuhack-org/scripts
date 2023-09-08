c165d82c37e65ce55062306e0f6c25cd69d0822c
NitroGuy10 <naviyer10@gmail.com>
2023-09-08 17:15:07 -0500

    Devpost team member identification script

---
+++ devpost_team_identifier/identify_teams.py
@@ -0,0 +1,148 @@
+# Team Identifier Script (for TAMUhack 2022 and beyond)
+# Naveen Iyer
+
+import json
+import csv
+import re
+
+print("-- TAMUhack Team Identifier Script --")
+
+def sanitize_and_load_json (file_name):
+    """Reads a JSON file, fixes unpleasant formatting, and returns a JSON object"""
+    with open(file_name, encoding="utf-8") as file:
+        contents = file.read()
+
+        # For some reason, when you export the database data as JSON,
+        # escape sequences are exported fine "\n" but backslashes are not escaped "\".
+        # The following takes care of that
+        contents = re.sub(r"\\[^bfnrtv'\"\\](?!\\)", "\\\\\\\\", contents)
+
+        # Also my FOSS database viewer exports JSON with a trailing comma like a weirdo
+        contents = re.sub(r"},\s*]", "}]", contents)
+
+        return json.loads(contents)
+
+
+# Read in application and user data
+print("Parsing application JSON...")
+applications = {}
+for application in sanitize_and_load_json("obos_application_application.json"):
+    applications[application["user_id"]] = application
+
+print("Parsing user JSON...")
+users = {}
+for user in sanitize_and_load_json("obos_user_user.json"):
+    users[user["id"]] = user
+
+for user_id in applications:
+    if user_id in users:
+        applications[user_id]["email"] = users[user_id]["email"]
+    else:
+        applications.pop(user_id)
+
+
+# Read in devpost submission data
+print("Parsing devpost CSV...")
+submissions = []
+with open("devpost.csv", encoding="utf-8") as file:
+    reader = csv.reader(file)
+
+    for row in reader:
+        # Skip the title row and skip teams without a submission
+        if row[1] == "Submission Url" or row[1] == "":
+            continue
+
+        submission = {
+            "title": row[0],
+            "url": row[1],
+            "categories": row[9].split(", "),
+            "members": [
+                {
+                    "firstName": row[11],
+                    "lastName": row[12],
+                    "email": row[13],
+                    "foundStatus": "",
+                    "checkedinStatus": ""
+                }
+            ]
+        }
+
+        for i in range(18, 18 + (3 * int(row[17])), 3):
+            submission["members"].append({
+                    "firstName": row[i],
+                    "lastName": row[i + 1],
+                    "email": row[i + 2],
+                    "foundStatus": "",
+                    "checkedinStatus": ""
+                })
+
+        submissions.append(submission)
+
+
+# See if team members appear in application data
+members_not_found = []
+members_not_checked_in = []
+print("Matching devpost data with application data...")
+for submission in submissions:
+    for member in submission["members"]:
+        member_application = None
+        for application in applications.values():  # See if emails appear
+            if member["email"].lower() == application["email"].lower():
+                member_application = application
+                break
+        if member_application is None:
+            for application in applications.values():  # See if full names appear
+                if member["firstName"].lower() == application["first_name"].lower() and member["lastName"].lower() == application["last_name"].lower():
+                    member_application = application
+                    break
+        if member_application is None:
+            member["foundStatus"] = "Not found in application data"
+            members_not_found.append(member)
+        
+        # Report if members are not "confirmed" or "checked in"
+        if member_application is not None:
+            # if not (member_application["status"] == "C" or member_application["status"] == "I"):
+            if not (member_application["status"] == "I"):
+                member["checkedinStatus"] = "Undesirable status: " + member_application["status"]
+                members_not_checked_in.append(member)
+
+
+# Output a csv file
+print("Creating output CSV file...")
+with open("team_info.csv", "w", encoding="utf-8", newline="") as out_file:
+    writer = csv.writer(out_file)
+
+    for submission in submissions:
+        writer.writerow([submission["title"], submission["url"]])
+        for i in range(0, max(len(submission["categories"]), len(submission["members"]))):
+            row = [""]
+
+            # List categories
+            if i < len(submission["categories"]):
+                row.append(submission["categories"][i])
+            else:
+                row.append("")
+            row.append("")
+
+            # List members
+            if i < len(submission["members"]):
+                member = submission["members"][i]
+                member_info = [member["firstName"], member["lastName"], member["email"], member["foundStatus"], member["checkedinStatus"]]
+                row += member_info
+
+            writer.writerow(row)
+        
+        writer.writerow([])
+        writer.writerow([])
+
+
+
+print("Done!\nOutput data in team_info.csv")
+
+print("\n\n\nThe following team members were not found in the application data!")
+for member in members_not_found:
+    print(f"{member['firstName']} {member['lastName']} - {member['email']}")
+
+print("\n\n\nThe following team members never checked in at the event!")
+for member in members_not_checked_in:
+    print(f"{member['firstName']} {member['lastName']} - {member['email']}")
