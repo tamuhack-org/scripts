99e5f4fb58c2207a4a0c01f1e82bed3f35ce3da0
NitroGuy10 <naviyer10@gmail.com>
2024-06-09 22:30:28 -0700

    goofy devpost is goofy

--- devpost_judging_attendance/devpost_judging_attendance.py
+++ devpost_judging_attendance/devpost_judging_attendance.py
@@ -1,11 +1,15 @@
 # Made by Naveen Iyer
 # 9 June 2024
 
+
 ATTENDANCE_FORM_RESPONSES_CSV_FILENAME = "attendance_form_responses.csv"  # Exported from the Google Forms responses spreadsheet
 DEVPOST_PROJECTS_FILENAME = "devpost.csv"  # Exported from devpost
 OUTPUT_FILENAME = "output.csv"
 
 import csv
+import requests
+from bs4 import BeautifulSoup
+from tqdm import tqdm
 
 present_urls = {}
 with open(ATTENDANCE_FORM_RESPONSES_CSV_FILENAME) as in_file:
@@ -18,14 +22,23 @@ with open(ATTENDANCE_FORM_RESPONSES_CSV_FILENAME) as in_file:
 
 projects = []
 num_submissions = 0
+print("Fetching project URLs from devpost...")
 with open(DEVPOST_PROJECTS_FILENAME) as in_file:
     reader = csv.DictReader(in_file)
-    for row in reader:
+    for row in tqdm(reader):
         num_submissions += 1
         submission_url = row["Submission Url"]
-        if submission_url in present_urls:
+
+        req = requests.get(submission_url)
+        soup = BeautifulSoup(req.text, "lxml")
+        software_url = soup.find("meta", property="og:url")
+        if software_url is None:
+            continue
+        software_url = software_url["content"]
+
+        if software_url in present_urls:
             projects.append(row)
-            present_urls[submission_url] = True
+            present_urls[software_url] = True
 
 with open(OUTPUT_FILENAME, "w") as out_file:
     writer = csv.DictWriter(out_file, fieldnames=projects[0].keys())
